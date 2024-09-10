93a7d935919db0bf1184a5f1e0f81533ba8d1da8
Yassinemo77 <uzii8712@gmail.com>
2024-09-10 20:33:23 +0200

    Update code.c

--- mini project/prjt1/code.c
+++ mini project/prjt1/code.c
@@ -163,3 +163,96 @@ int main()
 
     return 0;
 }
+/////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct etudiants{
+    int id;
+    char nom[100];
+    char prenom[100];
+    char dateDeNaissance[100];
+    char departement[100];
+    float noteGenerale;
+};
+
+struct etudiants etudiant[100];
+int idCounter  = 0;
+int numEtediant = 0;
+
+void menu(){
+    printf("\t 1- Ajouter un etudiant : \n");
+    printf("\t 2- modifier un etudiant : \n");
+    printf("\t 3- supprimer un etudiant : \n");
+    printf("\t 4- afficher les details d'un etudiant : \n");
+    printf("\t 5- afficher la moyenne generale  : \n");
+    printf("\t 6- afficher les statistiques : \n");
+}
+
+void ajouter(){
+    printf("entrer votre nom : ");
+    scanf(" %[^\n]s",&etudiant[numEtediant].nom);
+
+    printf("nentrer votre prenom : ");
+    scanf(" %[^\n]s",&etudiant[numEtediant].prenom);
+
+    printf("entrer la date de naissance : ");
+    scanf(" %[^\n]s",&etudiant[numEtediant].dateDeNaissance);
+
+    printf("entrer la departement : ");
+    scanf(" %[^\n]s",&etudiant[numEtediant].departement);
+
+    printf("entrer la note generale : ");
+    scanf("%.2f",&etudiant[numEtediant].noteGenerale);
+
+    etudiant[numEtediant].id = idCounter++;
+
+    numEtediant++;
+}
+
+void rechercherParLeNom(){
+    char nom[100];
+    printf("entrer le nom de l'etudiant");
+    scanf("%s",&nom);
+
+    for(int j = 0 ; j < numEtediant ; j++){
+        int res = strcmp(etudiant[numEtediant].nom, nom);
+
+        if(res == 0){
+                printf("le nom : %s : ", etudiant[j].nom);
+
+                printf("entrer votre prenom : %s",etudiant[j].prenom);
+
+                printf("entrer la date de naissance : %s", etudiant[j].dateDeNaissance);
+
+                printf("entrer la departement : %s",etudiant[j].departement);
+
+                printf("entrer la note generale : %s", etudiant[j].noteGenerale);
+
+                printf("entrer la note generale : %s", etudiant[j].noteGenerale);
+        }
+    }
+}
+
+int main()
+{
+    int choix;
+    do{
+        menu();
+        printf("entrer un choix : ");
+        scanf("%d",&choix);
+
+        switch(choix){
+        case 1 :
+            ajouter();
+            break;
+        case 2 :
+            rechercherParLeNom();
+            break;
+        }
+
+    }while(choix < 4);
+
+    return 0;
+}
